376a7063548c071530a92a04c05b449d66251c96
Thor Ng <Tho>
2021-02-21 15:39:41 +0700

    update interfac of dynamo

--- storage/dynamodb/dynamodb.go
+++ storage/dynamodb/dynamodb.go
@@ -6,10 +6,10 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
-
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
 )
 
-func Connection() (*dynamodb.DynamoDB, error) {
+func Connection() (dynamodbiface.DynamoDBAPI, error) {
 	region := local.Getenv("AWS_REGION")
 	awsSession, err := session.NewSession(&aws.Config{
 		Region: aws.String(region),
