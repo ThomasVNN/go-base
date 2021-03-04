4d92626e69718099e891ff7ca4cce0deb1669a4d
Thor Ng <Tho>
2021-03-04 09:01:58 +0700

    remove wanring log .env

--- storage/local/local.go
+++ storage/local/local.go
@@ -1,15 +1,13 @@
 package local
 
 import (
-	"github.com/joho/godotenv"
-	logg "log"
 	"os"
 )
 
 func Getenv(key string) string {
-	err := godotenv.Load()
-	if err != nil {
-		logg.Print("Error loading .env file")
-	}
+	//err := godotenv.Load()
+	//if err != nil {
+	//	logg.Print("Error loading .env file")
+	//}
 	return os.Getenv(key)
 }
