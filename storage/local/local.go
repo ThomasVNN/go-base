28050a82ccee01681d36977e142afaf53f2f6b4e
Thor Ng <Tho>
2021-03-04 10:49:18 +0700

    fix bug cannot load env

--- storage/local/local.go
+++ storage/local/local.go
@@ -1,13 +1,14 @@
 package local
 
 import (
+	"github.com/joho/godotenv"
 	"os"
 )
 
 func Getenv(key string) string {
-	//err := godotenv.Load()
-	//if err != nil {
+	err := godotenv.Load()
+	if err != nil {
 	//	logg.Print("Error loading .env file")
-	//}
+	}
 	return os.Getenv(key)
 }
